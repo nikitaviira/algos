98e772f25fa589f345d82baea0188aaed368afba
Nikita Viira <nikita.viira@osom.finance>
2022-10-20 14:57:06 +0300

    Add invert binary tree task

---
+++ src/com/company/InvertBinaryTree.java
@@ -0,0 +1,59 @@
+package com.company;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+// https://leetcode.com/problems/invert-binary-tree/
+public class InvertBinaryTree {
+  public static void printTree(TreeNode node, String prefix) {
+    if (node == null) return;
+
+    System.out.println(prefix + node.val);
+    printTree(node.left , prefix + " ");
+    printTree(node.right , prefix + " ");
+  }
+
+  public static void main(String[] args) {
+    printTree(invertTree(new TreeNode(4, new TreeNode(2, new TreeNode(1), new TreeNode(3)),
+      new TreeNode(7, new TreeNode(6), new TreeNode(9)))), "");
+  }
+
+  public static TreeNode invertTree(TreeNode root) {
+    if (root == null || (root.left == null && root.right == null)) return root;
+
+    Queue<TreeNode> queue = new LinkedList<>();
+    queue.add(root);
+
+    while (!queue.isEmpty()) {
+      var size = queue.size();
+      for (int i = 0; i < size; i++) {
+        TreeNode temp = queue.poll();
+
+        TreeNode left = temp.left;
+        if (temp.left != null) queue.add(temp.left = temp.right);
+        if (temp.right != null) queue.add(temp.right = left);
+      }
+    }
+
+    return root;
+  }
+
+  public static class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+      this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+      this.val = val;
+      this.left = left;
+      this.right = right;
+    }
+  }
+}
